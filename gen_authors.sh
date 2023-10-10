21d3d700a6754cd722db5bcc02ce24e533404726
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-10-10 14:27:44 -0700

    authors file

---
+++ gen_authors.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Set the path to the Git repository
+REPO_PATH="/AirBnB_clone"
+
+# Set the path to the AUTHORS file
+AUTHORS_FILE="AUTHORS"
+
+# Generate unique authors and emails from Git commit history
+git -C "$REPO_PATH" log '--format=%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf > "$AUTHORS_FILE"
+
+echo "Generated $AUTHORS_FILE"
