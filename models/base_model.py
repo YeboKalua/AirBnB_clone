a3019f6918e5d091bd4bc76a3030e16c0194e579
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-10-12 15:26:18 +0100

    adding kwarg

--- models/base_model.py
+++ models/base_model.py
@@ -32,9 +32,9 @@ class BaseModel():
         if kwargs:
             for keys,vals in kwargs.items():
                 if keys != '__class__':
-                    if keys == 'created_at' or keys == 'updated_at' and type(vals) == 'str':
+                    if (keys == 'created_at' or keys == 'updated_at') and type(vals) == 'str':
                         vals_datetime = datetime.strptime(vals, '%Y-%m-%dT%H:%M:%S.%f')
-                        self.keys = vals_datetime.strftime("datetime.datetime(%Y, %m, %d, %H, %M, %S, %f)")
+                        setattr(self, keys, vals_datetime)
                     setattr(self, keys, vals)
         else:
             self.id = self.__class__.id
