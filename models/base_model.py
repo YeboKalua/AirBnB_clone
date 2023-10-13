0a53dcc34b218e131937f172ece0cd07862378d1
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-10-13 23:27:51 +0100

    storage7

--- models/base_model.py
+++ models/base_model.py
@@ -29,9 +29,9 @@ class BaseModel():
     
     def __init__(self, *args, **kwargs):
         """To initialize class attributes"""
-        id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for keys,vals in kwargs.items():
                 if keys != '__class__':
