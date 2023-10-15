cc361eee9c62b79a6869135eb980875f85bb5c27
Yebo Kalua <kaluayebo2@gmail.com>
2023-10-15 17:39:57 +0000

    revisiting fstring

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,7 +39,7 @@ class FileStorage():
 
     def new(self, obj):
         """sets in new value into objects"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
